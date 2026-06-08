40c0df8c4a6c921d961a104e2705e53f320a5e24
Vaishnavi <vcode8963@gmail.com>
2026-06-08 13:26:30 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        smap={}
+        tmap={}
+        
+        for i in s:
+            smap[i] = smap.get(i,0)+1
+        
+        for i in t:
+            tmap[i] = tmap.get(i,0)+1
+
+        for i in smap:
+            if smap[i] != tmap.get(i,0):
+                return False
+        return True
